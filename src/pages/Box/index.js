1a407112da2a47bdc19c1ff7fe048048990b47c9
Renato Rocha <renatob.rocha@gmail.com>
2019-04-12 02:45:13 -0300

    Aplicaçao front-end

---
+++ src/pages/Box/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { distanceInWords } from 'date-fns';
+import { pt } from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
+import socket from 'socket.io-client'; 
+
+import api from '../../services/api'
+
+import { MdInsertDriveFile } from "react-icons/md";
+import logo from '../../assets/logo.svg';
+import './styles.css';
+
+export default class Box extends Component {
+
+    state = { box: {}}
+
+    async componentDidMount() {
+        this.subscribeToNewFile();
+        const box = this.props.match.params.id;
+        const response = await api.get(`/boxes/${box}/files`);
+        this.setState({box: response.data})
+    }
+
+    subscribeToNewFile = () => {
+        const box = this.props.match.params.id;
+        const io = socket('https://omnistack-backend-api.herokuapp.com');
+        io.emit('connectionRoom', box);
+        io.on('file', data => {
+            this.setState({ box: {...this.state.box, files: [data, ...this.state.box.files]}})
+        });
+    }
+
+    handleUpload = (files) => {
+        files.forEach(file => {
+            const box = this.props.match.params.id;
+            const data = new FormData();
+            data.append('file', file);
+            api.post(`boxes/${box}/files`, data);
+        });
+    }
+    render() {
+        return (
+            <div id="box-container">
+                <header>
+                    <img src={logo} alt=""/>
+                    <h1>{this.state.box.title}</h1>
+                </header>
+
+                <Dropzone onDropAccepted={this.handleUpload}>
+                    {
+                        ({getRootProps, getInputProps}) => (
+                            <div className="upload" {...getRootProps()}>
+                                <input {...getInputProps()}></input>
+
+                                <p>Arraste arquivos ou clique aqui</p>
+                            </div> 
+                        )
+                    }
+                </Dropzone>
+                <ul>
+                    {
+                        this.state.box.files && this.state.box.files.map(file => (
+                        <li key={file._id}>
+                            <a className="fileinfo" href={file.url} target="_blank">
+                                <MdInsertDriveFile size={24} color="#A5Cfff"></MdInsertDriveFile>
+                                <strong>{file.title}</strong>
+                            </a>
+
+                            <span>há {distanceInWords(file.createdAt, new Date(), {
+                                locale: pt
+                            })}</span>
+                        </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
